c35b0901792d7ee2f14a03ffd3debe2b0df7f293
Rodica Trifan <trodicaro@gmail.com>
2017-12-02 17:25:30 -0600

    Add controller for projects with views. Start on project index page.
    Add controller for projects
    
    And views

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,12 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+
+  def show
+    # @auth_token = AutodeskToken.get
+  end
+
+  def new
+
+  end
+end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,12 @@
 class ReviewsController < ApplicationController
   def index
-    
+
   end
 
   def show
-    @auth_token = AutodeskToken.get
   end
 
   def new
-    
+
   end
 end
