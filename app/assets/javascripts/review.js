a01293316ba0e90e3ce34f2cec52eb33e825f462
TJ <tj@buildout.com>
2017-12-02 17:32:37 -0500

    DIFF

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,83 +1,123 @@
 $(function() {
-  if($(".reviews-show").length == 0) return;
+  var modelA = "";
+  var modelB = "";
 
-  var viewer2d;
-  var viewer3d;
-  var options = {
+  window.viewer = null;
+  window.viewer2d = null;
+  var lmvDoc;
+
+  function launchViewer(urn) {
+    var options = {
       env: 'AutodeskProduction',
-      accessToken: authToken
-  };
-  var documentId = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Zm9yZ2UtamF2YS1zYW1wbGUtYXBwLTdpdGw0a2dobmticGZhNjdzcTZxbGRla3d0dHJtYmR2L215LXNhbXBsdC5ydnQ';
-  Autodesk.Viewing.Initializer(options, function onInitialized(){
+      accessToken: "eyJhbGciOiJIUzI1NiIsImtpZCI6Imp3dF9zeW1tZXRyaWNfa2V5In0.eyJjbGllbnRfaWQiOiI3SVRMNEtnSG5LQlBGQTY3U3E2UWxkRUtXdFRSbUJEViIsImV4cCI6MTUxMjI1NTQxNiwic2NvcGUiOlsiZGF0YTpyZWFkIiwiZGF0YTp3cml0ZSIsImJ1Y2tldDpjcmVhdGUiXSwiYXVkIjoiaHR0cHM6Ly9hdXRvZGVzay5jb20vYXVkL2p3dGV4cDYwIiwianRpIjoic28xSm8xS0JiMG55Sm0wRnlhMnlDVWdpZERub2VUd203ZUFDbHhuMGxONzZCQ09xY3N1QUFZQXEyRU1ES20xNSJ9.ha9gffOQY-Lvntcr-9R_bX-Wpl6CvI41lDwVGgfl-8Y"
+    };
+    var documentId = urn;
+    if (viewer == null) {
+      Autodesk.Viewing.Initializer(options, function onInitialized() {
+        // Create Viewer instance and load model.
+        var viewerDiv = document.getElementById('Viewer3dDiv');
+        viewer = new Autodesk.Viewing.Private.GuiViewer3D(viewerDiv);
+        var errorCode = viewer.start();
+
+        viewer2d = new Autodesk.Viewing.Private.GuiViewer3D(document.getElementById('Viewer2dDiv'));
+        viewer2d.start();
+
+        // Check for initialization errors.
+        if (errorCode) {
+          console.error('viewer.start() error - errorCode:' + errorCode);
+          return;
+        }
+
+        Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+      });
+    }
+    else {
       Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
-  });
+    }
+  }
 
   /**
-  * Autodesk.Viewing.Document.load() success callback.
-  * Proceeds with model initialization.
-  */
+   * Autodesk.Viewing.Document.load() success callback.
+   * Proceeds with model initialization.
+   */
   function onDocumentLoadSuccess(doc) {
-      window.d = doc;
-      // A document contains references to 3D and 2D viewables.
-      var viewables2d = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRootItem(), {'type':'geometry', 'role': '2d'}, true);
-      var viewables3d = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRootItem(), {'type':'geometry', 'role': '3d'}, true);
-      if (viewables2d.length === 0 || viewables3d === 0) {
-          console.error('Document contains no viewables.');
-          return;
-      }
+    // A document contains references to 3D and 2D viewables.
+    var viewables = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRootItem(), {'type': 'geometry', 'role': '3d'}, true);
+    if (viewables.length === 0) {
+      console.error('Document contains no viewables.');
+      return;
+    }
 
-      // Choose any of the avialble viewables
-      var initialViewable2d = viewables2d[0];
-      var initialViewable3d = viewables3d[0];
-      var svfUrl2d = doc.getViewablePath(initialViewable2d);
-      var svfUrl3d = doc.getViewablePath(initialViewable3d);
-      var modelOptions = {
-          sharedPropertyDbPath: doc.getPropertyDbPath()
-      };
+    var initialViewable = viewables[0];
+    var svfUrl = doc.getViewablePath(initialViewable);
+    var modelOptions = {
+      sharedPropertyDbPath: doc.getPropertyDbPath()
+    };
+    viewer.loadModel(svfUrl, modelOptions, onLoadModelSuccess, onLoadModelError);
 
-      var viewerDiv2d = document.getElementById('Viewer2dDiv');
-      var viewerDiv3d = document.getElementById('Viewer3dDiv');
-      viewer2d = new Autodesk.Viewing.Private.GuiViewer3D(viewerDiv2d);
-      viewer2d.start(svfUrl2d, modelOptions, onLoadModelSuccess, onLoadModelError);
-      viewer3d = new Autodesk.Viewing.Private.GuiViewer3D(viewerDiv3d);
-      viewer3d.start(svfUrl3d, modelOptions, onLoadModelSuccess, onLoadModelError);
 
-      console.log(viewables2d);
-      $.each(viewables2d, function(index, viewable) {
+    // A document contains references to 3D and 2D viewables.
+    viewables = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRootItem(), {'type': 'geometry'}, true);
+    window.viewables2d = viewables;
+    if (viewables.length === 0) {
+      console.error('Document contains no viewables.');
+      return;
+    }
+
+    initialViewable = viewables[2];
+    svfUrl = doc.getViewablePath(initialViewable);
+    modelOptions = {
+      sharedPropertyDbPath: doc.getPropertyDbPath()
+    };
+    viewer2d.loadModel(svfUrl, modelOptions, onLoadModel2dSuccess, onLoadModelError);
+
+    if(!window.loaded2d) {
+      window.loaded2d = true;
+      $.each(viewables, function(index, viewable) {
           $('#SheetPane').append('<li><a href="#" data-viewable="'+ index +'" class="sheet-link">'+ viewable["name"] +'</a></li>');
       });
 
       $('.sheet-link').on('click', function(e) {
         var sheetIdx = $(this).attr('data-viewable');
-        var newSvfUrl = doc.getViewablePath(viewables2d[sheetIdx]);
+        var newSvfUrl = doc.getViewablePath(window.viewables2d[sheetIdx]);
         viewer2d.loadModel(newSvfUrl, modelOptions, onLoadModelSuccess, onLoadModelError);
-        console.log('jeuy')
         e.preventDefault();
       });
+    }
   }
 
   /**
    * Autodesk.Viewing.Document.load() failuire callback.
    */
-  function onDocumentLoadFailure(viewerErrorCode) {
-      console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode);
-  }
+  function onDocumentLoadFailure(viewerErrorCode) {}
 
   /**
    * viewer.loadModel() success callback.
    * Invoked after the model's SVF has been initially loaded.
    * It may trigger before any geometry has been downloaded and displayed on-screen.
    */
+  var models = [];
   function onLoadModelSuccess(model) {
-      console.log('onLoadModelSuccess()!');
-      console.log(model);
+    models.push(model);
+    if (models.length==2)
+      window.viewer.loadExtension('Autodesk.Forge.Samples.VersionChanges', {'modelA': models[0], 'modelB': models[1], 'viewer': window.viewer});
   }
+
+  var models2d = [];
+  function onLoadModel2dSuccess(model) {
+    models2d.push(model);
+    if (models2d.length==2)
+      window.viewer2d.loadExtension('Autodesk.Forge.Samples.VersionChanges', {'modelA': models[0], 'modelB': models[1], 'viewer': window.viewer2d});
+  }
+
 
   /**
    * viewer.loadModel() failure callback.
    * Invoked when there's an error fetching the SVF file.
    */
-  function onLoadModelError(viewerErrorCode) {
-      console.error('onLoadModelError() - errorCode:' + viewerErrorCode);
-  }
+  function onLoadModelError(viewerErrorCode) {}
+
+  launchViewer('urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Zm9yZ2UtamF2YS1zYW1wbGUtYXBwLTdpdGw0a2dobmticGZhNjdzcTZxbGRla3d0dHJtYmR2L215LXNhbXBsdC5ydnQ');
+  launchViewer("urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Zm9yZ2UtamF2YS1zYW1wbGUtYXBwLTdpdGw0a2dobmticGZhNjdzcTZxbGRla3d0dHJtYmR2L215LXNhbXBsZS1vbGQucnZ0");
+
 });
